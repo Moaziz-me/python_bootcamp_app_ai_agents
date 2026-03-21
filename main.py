d724aa4362f05e5a8bf8609500bcc2d1e70f8556
Mohammad Aziz <100340450+Moaziz-me@users.noreply.github.com>
2026-03-21 14:13:41 +1100

    updating last commit.

--- main.py
+++ main.py
@@ -1,16 +1,3 @@
-
-# user_prompt = "Enter todo1: "
-#
-#
-#
-# todos = []  # create list ONCE
-#
-# while True:
-#     todo = input(user_prompt)
-#     todos.append(todo.capitalize())  # add new item
-#     print(todos)
-
-
 todos = []
 
 while True:
