b65aa4eefcd486d810224865ee58db274a93071e
Mohammad Aziz <100340450+Moaziz-me@users.noreply.github.com>
2026-03-21 14:03:05 +1100

    first commit.

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 
-user_prompt = "Enter todo1: "
+# user_prompt = "Enter todo1: "
+#
+#
+#
+# todos = []  # create list ONCE
+#
+# while True:
+#     todo = input(user_prompt)
+#     todos.append(todo.capitalize())  # add new item
+#     print(todos)
 
-
-
-todos = []  # create list ONCE
+todos = []
 
 while True:
-    todo = input(user_prompt)
-    todos.append(todo.capitalize())  # add new item
-    print(todos)
+    user_action = input("Type add or show: ")
+
+    match user_action:
+        case "add":
+            todos.append(user_action)
+        case "show":
+            print(todos)
