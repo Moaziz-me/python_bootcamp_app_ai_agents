ae6fc1867689171f2525b95a81de9870c7adb650
Mohammad Aziz <100340450+Moaziz-me@users.noreply.github.com>
2026-03-21 15:45:30 +1100

    end of the day commit.

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ todos = []
 
 while True:
     user_action = input("Type add, show or exit: ")
+    user_action = user_action.strip()
 
     match user_action:
         case "add":
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case "show":
-            print(todos)
+        case "show" | "display":
+            for item in todos:
+                item = item.capitalize()
+                print(item)
+        case "edit":
+            number = int(input("Got it, number of todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new to do: ")
+            todos[number] = new_todo
         case "exit":
             break
+        case whatever:
+            print("Hey it does not match any of the cases")
 print("Bye")
