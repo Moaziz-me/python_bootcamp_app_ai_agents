31ff71595212823e7ce052fc2222a29ac4ce86ac
Mohammad Aziz <100340450+Moaziz-me@users.noreply.github.com>
2026-04-16 10:09:54 +1000

    optimize main py start of the day

--- main.py
+++ main.py
@@ -2,28 +2,32 @@ while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
-        todo = input("Enter a todo: ") + "\n"
+    if user_action.startswith("add"):
+        todo = user_action[4:] + '\n'
+        # single \n create a line before and after the code
 
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
-        todos.append(todo)
+        todos.append(todo+"\n")
 
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
-    if 'show' in user_action:
+    elif user_action.startswith("show"):
 
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
 
             for index, item in enumerate(todos):
                 item = item.strip('\n')
-                row = f"{index + 1} -  {item}"
+                row = f"{index + 1}-{item}"
                 print(row)
-    if 'edit'  in user_action:
-        number = int(input("Number of the todo to edit: "))
+
+    elif user_action.startswith("edit"):
+        number = int(user_action[5:])
+        print(number)
+
         number = number - 1
 
         with open('todos.txt', 'r') as file:
@@ -35,12 +39,12 @@ while True:
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
 
-    if 'complete' in user_action:
-        number = int(input("Number of the todo to complete: "))
+    elif user_action.startswith("complete"):
+        number = int(user_action[9:])
 
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
-        indes = number - 1
+        index = number - 1
         todo_to_remove = todos[index].strip('\n')
         todos.pop(index)
 
@@ -50,7 +54,7 @@ while True:
         message = f"Todo {todo_to_remove} was removed from the list"
         print(message)
 
-    if 'exit' in user_action:
+    elif user_action.startswith("exit"):
         break
     
 print("Bye")
