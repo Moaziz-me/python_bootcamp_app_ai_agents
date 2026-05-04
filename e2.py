63e8e82a87301bb9a1e8d9c873cff9808ddac8d0
Mohammad Aziz <100340450+Moaziz-me@users.noreply.github.com>
2026-05-04 12:18:49 +1000

    4.5.2026 json file structure

--- e2.py
+++ e2.py
@@ -2,17 +2,19 @@ import csv
 
 with open("file/weather.csv", "r") as file:
     df_weather = list(csv.reader(file))
-
+print(df_weather)
 while True:
     city_input = input("Enter city: ")
 
     result = False
 
     for row in df_weather:
-        if row[0] == city_input:
+        if row[0] == city_input.title():
             print(row[1])
             result = True
 
     if not result:
         print("City not found")
+
+
 
---
+++ e3.py
@@ -0,0 +1,24 @@
+import json
+
+with open("questions.json", "r") as file:
+    data = file.read()
+
+data = json.loads(data)
+
+
+score = 0
+for q in data:
+    print(q["question_text"])
+    for i, a in enumerate(q["alternatives"]):
+        print(i+1, "-", a)
+    user_choice = int(input("Enter your choice: "))
+    q["user_choice"] = user_choice
+
+    if q["user_choice"] == q["correct_answer"]:
+        score = score + 1
+
+for q in data:
+    message = f"Your answer: {q['user_choice']},"\
+            f"Correct Answer: {q['correct_answer']}"
+    print(message)
+print(score, "/", len(data))
