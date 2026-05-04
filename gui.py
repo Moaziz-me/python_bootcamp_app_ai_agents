804b06f6a11b9b7810cedbb220e294fa7bb9b23f
Mohammad Aziz <100340450+Moaziz-me@users.noreply.github.com>
2026-05-04 13:35:11 +1000

    GUI app initial setup - add feature!

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import functions
 import FreeSimpleGUI as sg
 
-Window = sg.Window("My To-do App", layout=[""])
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="input_box")
+add_button = sg.Button("Add")
+
+Window = sg.Window("My To-do App", layout=[[label], [input_box, add_button]])
 Window.read()
 Window.close()
