98cf2eeda61be47ec54c7b50161215f352d90f08
Mohammad Aziz <100340450+Moaziz-me@users.noreply.github.com>
2026-05-13 17:10:49 +1000

    start of the day May 13th 2026!

--- gui.py
+++ gui.py
@@ -8,6 +8,18 @@ add_button = sg.Button("Add")
 Window = sg.Window("My To-do App",
                    layout=[[label], [input_box, add_button]],
                    font=('Helvetica', 20),)
-event = Window.read()
-print(event)
+
+while True:
+    event, values = Window.read()
+    print(event)
+    print(values)
+
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['Todo'] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
 Window.close()
