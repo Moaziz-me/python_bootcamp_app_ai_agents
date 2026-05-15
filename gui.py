573fd68ef83ae164b1b968acf71cef4f40e53b7a
Mohammad Aziz <100340450+Moaziz-me@users.noreply.github.com>
2026-05-15 14:18:50 +1000

    start of the day May 13th 2026!

--- gui.py
+++ gui.py
@@ -5,8 +5,16 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="input_box", key='Todo')
 add_button = sg.Button("Add")
 
+list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+                      enable_events=True, size=(45, 10))
+
+edit_button = sg.Button("Edit")
+
 Window = sg.Window("My To-do App",
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label],
+                           [input_box, add_button, edit_button],
+                           [list_box]
+                           ],
                    font=('Helvetica', 20),)
 
 while True:
@@ -20,6 +28,24 @@ while True:
             new_todo = values['Todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+
+            Window["todos"].update(values=todos)
+
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['Todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            Window["todos"].update(values=todos)
+
+        case 'Todo':
+            Window["Todo"].update(values=values['todos'])
+
         case sg.WIN_CLOSED:
             break
+
+
 Window.close()
